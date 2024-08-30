3160316d973e25db449ebe3be4f7d9a66562db06
ChrisSolan <chrissolanopoly@gmail.com>
2024-08-29 20:17:17 -0400

    Add a Finish Day button that clears the calorie and macro tracking
    Will change the name of the button later

--- frontend/src/Contexts/FoodContext.js
+++ frontend/src/Contexts/FoodContext.js
@@ -1,31 +1,43 @@
 //This Context file will allow all the components to use the states and functions below
-    //Makes it easy to track total calories and macros from various pages
+//Makes it easy to track total calories and macros from various pages
 import { useState, createContext, useContext } from "react";
 
 const FoodContext = createContext();
 
 export const FoodProvider = ({ children }) => {
-    
-    const [consumedItems, setConsumedItems] = useState([]);
-    const [calories, setCalories] = useState(0);
-    const [carbs, setCarbs] = useState(0);
-    const [protein, setProtein] = useState(0);
-    const [fat, setFat] = useState(0);
+  const [consumedItems, setConsumedItems] = useState([]);
+  const [calories, setCalories] = useState(0);
+  const [carbs, setCarbs] = useState(0);
+  const [protein, setProtein] = useState(0);
+  const [fat, setFat] = useState(0);
 
-    const addFoodItem = (foodItem) => {
-        //takes a state as an arugment (prevItems in this case), then spreads it into a new state or array and appends the foodItem at the end
-        setConsumedItems((consumed) => [...consumed, foodItem]); //might need to be curly braces instead of brackets
-        setCalories((calories) => calories + foodItem.calories);
-        setCarbs((carbs) => [carbs + foodItem.macros.carbs]);
-        setProtein((protein) => [protein + foodItem.macros.protein]);
-        setFat((fat) => [fat + foodItem.macros.fat]);
-
-    }
-    return (
-        <FoodContext.Provider value={{consumedItems, calories, carbs, protein, fat, addFoodItem}}>
-            {children}
-        </FoodContext.Provider>
-    );
+  const addFoodItem = (foodItem) => {
+    //takes a state as an arugment (prevItems in this case), then spreads it into a new state or array and appends the foodItem at the end
+    setConsumedItems((consumed) => [...consumed, foodItem]); //might need to be curly braces instead of brackets
+    setCalories((calories) => calories + foodItem.calories);
+    setCarbs((carbs) => carbs + foodItem.macros.carbs);
+    setProtein((protein) => protein + foodItem.macros.protein);
+    setFat((fat) => fat + foodItem.macros.fat);
+  };
+  return (
+    <FoodContext.Provider
+      value={{
+        consumedItems,
+        calories,
+        carbs,
+        protein,
+        fat,
+        setConsumedItems,
+        setCalories,
+        setCarbs,
+        setProtein,
+        setFat,
+        addFoodItem,
+      }}
+    >
+      {children}
+    </FoodContext.Provider>
+  );
 };
 
 export const useFoodContext = () => useContext(FoodContext);
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -7,11 +7,17 @@ import { useFoodContext } from '../Contexts/FoodContext';
 export const Home = () => { //maybe have a setCategory state var for knowing if a food is for breakfast or other...
     const navigate = useNavigate();
     //the state names have to be the same as the exported states from useAddFoodItem
-    const { calories, carbs, protein, fat } = useFoodContext();
+    const { calories, carbs, protein, fat, setCalories, setCarbs, setProtein, setFat } = useFoodContext();
 
-
+    const handleCalories = () => {
+        setCalories(0);
+        setCarbs(0);
+        setProtein(0);
+        setFat(0);
+    }
     return (
         <div>
+            <button onClick={handleCalories}>Finish Day</button>
             <h1>Calories: {calories} cal</h1>
             <p>Carbs: {carbs}g</p>          
             <p>Protein: {protein}g</p>
